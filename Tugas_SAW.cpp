0825a274548a47bcb8d2d0e0ad2a38a468f96817
kendi ardi <arsamisapper12@gmail.com>
2017-12-30 21:40:21 +0800

    Double 3

--- Tugas_SAW.cpp
+++ Tugas_SAW.cpp
@@ -100,9 +100,8 @@ int main(){
 			
 	double ha[3]={
 	RWa1c[0]+RWa1c[1]+RWa1c[2]+RWa1c[3]+RWa1c[4],
-	RWa2c[0]+RWa2c[1]+RWa2c[2]+RWa2c[3]+RWa2c[4],	
-	
-	};											
+	RWa2c[0]+RWa2c[1]+RWa2c[2]+RWa2c[3]+RWa2c[4],
+	RWa3c[0]+RWa3c[1]+RWa3c[2]+RWa3c[3]+RWa3c[4]};												
 	
 	
 }
