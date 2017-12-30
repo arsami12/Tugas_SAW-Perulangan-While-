86212ac59c56e7e73445fa613620fbb01604f1e6
kendi ardi <arsamisapper12@gmail.com>
2017-12-30 21:30:14 +0800

    Menampilkan Matriks R * W

--- Tugas_SAW.cpp
+++ Tugas_SAW.cpp
@@ -77,7 +77,11 @@ int main(){
 	
 	double ha1[5]={ (RWa1c[0]+RWa1c[1]+RWa1c[2]+RWa1c[3]+RWa1c[4]) };
 	double ha2[5]={ (RWa2c[0]+RWa2c[1]+RWa2c[2]+RWa2c[3]+RWa2c[4]) };
-	double ha3[5]={ (RWa3c[0]+RWa3c[1]+RWa3c[2]+RWa3c[3]+RWa3c[4]) };					
+	double ha3[5]={ (RWa3c[0]+RWa3c[1]+RWa3c[2]+RWa3c[3]+RWa3c[4]) };
+	
+	//Menampilkan Matriks R * W
+	cout << endl;
+	cout << "MATRIKS R * W :"<<endl;					
 	
 	
 }
