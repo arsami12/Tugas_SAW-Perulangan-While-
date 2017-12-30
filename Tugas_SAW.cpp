9180f8fb8fc02bb29873e71dc9d6e5a5a2d1b49b
kendi ardi <arsamisapper12@gmail.com>
2017-12-30 21:41:27 +0800

    Menampilkan Nilai Akhir

--- Tugas_SAW.cpp
+++ Tugas_SAW.cpp
@@ -103,5 +103,7 @@ int main(){
 	RWa2c[0]+RWa2c[1]+RWa2c[2]+RWa2c[3]+RWa2c[4],
 	RWa3c[0]+RWa3c[1]+RWa3c[2]+RWa3c[3]+RWa3c[4]};												
 	
-	
+	//Menampilkan Nilai Akhir
+	cout << endl;
+	cout << "HASIL AKHIR  :"<<endl;
 }
